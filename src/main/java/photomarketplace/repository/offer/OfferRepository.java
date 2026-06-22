00e9366c0b47efb02142cd5d4fdb93064e268455
SveRaTom <98812004+SveRaTom@users.noreply.github.com>
2026-06-22 22:24:05 +0300

    refactor: add find all by photographer id method in offer repository

--- src/main/java/photomarketplace/repository/offer/OfferRepository.java
+++ src/main/java/photomarketplace/repository/offer/OfferRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import photomarketplace.model.entity.offer.Offer;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, UUID> {
 
+    List<Offer> findAllByPhotographerId(UUID photographerId);
 }
