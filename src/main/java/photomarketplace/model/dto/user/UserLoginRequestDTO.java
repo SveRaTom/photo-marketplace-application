2e9673ca731d3c72a1c5c3e709fd4b38f4b808f7
SveRaTom <98812004+SveRaTom@users.noreply.github.com>
2026-06-22 13:54:59 +0300

    refactor: add validation messages in user login request dto and user register request dto

--- src/main/java/photomarketplace/model/dto/user/UserLoginRequestDTO.java
+++ src/main/java/photomarketplace/model/dto/user/UserLoginRequestDTO.java
@@ -1,6 +1,7 @@
 package photomarketplace.model.dto.user;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,11 @@ import lombok.Data;
 @Data
 public class UserLoginRequestDTO {
 
+    @NotBlank(message = "Email must not be blank")
     @Email(message = "Email should be valid")
     private String email;
 
+    @NotBlank(message = "Password must not be blank")
     @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }
--- src/main/java/photomarketplace/model/dto/user/UserRegisterRequestDTO.java
+++ src/main/java/photomarketplace/model/dto/user/UserRegisterRequestDTO.java
@@ -1,5 +1,6 @@
 package photomarketplace.model.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import photomarketplace.model.entity.user.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -11,12 +12,15 @@ import lombok.Data;
 @Data
 public class UserRegisterRequestDTO {
 
+    @NotBlank(message = "Username must not be blank")
     @Size(min = 6, message = "Username must be at least 6 characters")
     private String username;
 
+    @NotBlank(message = "Email must not be blank")
     @Email(message = "Email should be valid")
     private String email;
 
+    @NotBlank(message = "Password must not be blank")
     @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
