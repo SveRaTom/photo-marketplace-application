63808f88d68024e0151abb334992face6b60abc8
SveRaTom <98812004+SveRaTom@users.noreply.github.com>
2026-06-23 12:31:56 +0300

    refactor: add methods in photo repository

--- src/main/java/photomarketplace/repository/photo/PhotoRepository.java
+++ src/main/java/photomarketplace/repository/photo/PhotoRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import photomarketplace.model.entity.photo.Photo;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, UUID> {
 
+    List<Photo> findAllByOfferPhotographerIdOrderByCreatedAtDesc(UUID photographerId);
+
+    List<Photo> findAllByOfferIdOrderByCreatedAtDesc(UUID offerId);
 }
