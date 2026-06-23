ae2d92c8c587a6f3626354be37cb49d79dac9fa2
SveRaTom <98812004+SveRaTom@users.noreply.github.com>
2026-06-23 12:19:23 +0300

    refactor: add methods in review repository

--- src/main/java/photomarketplace/repository/review/ReviewRepository.java
+++ src/main/java/photomarketplace/repository/review/ReviewRepository.java
@@ -4,9 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import photomarketplace.model.entity.review.Review;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
+    List<Review> findAllByAuthorIdOrderByCreatedAtDesc(UUID authorId);
+
+    List<Review> findAllByOfferPhotographerIdOrderByCreatedAtDesc(UUID photographerId);
+
+    List<Review> findAllByOfferIdOrderByCreatedAtDesc(UUID offerId);
+
+    boolean existsByBookingId(UUID bookingId);
 }
