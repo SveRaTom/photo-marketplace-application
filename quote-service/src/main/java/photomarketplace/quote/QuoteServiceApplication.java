af0b2473da7fdf59e4621a828419b4aa9fb5a9c6
SveRaTom <98812004+SveRaTom@users.noreply.github.com>
2026-08-06 00:57:59 +0300

    chore: scaffold quote service

---
+++ quote-service/src/main/java/photomarketplace/quote/QuoteServiceApplication.java
@@ -0,0 +1,12 @@
+package photomarketplace.quote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(QuoteServiceApplication.class, args);
+    }
+}
